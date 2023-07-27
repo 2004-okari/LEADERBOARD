db2130fd666c91aa48e1828da3a59d55f70e4629
okari <nyandika15@gmail.com>
2023-07-27 17:47:45 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import './styles.css';
 
-const API_URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
 const gameName = { name: 'Okari Game' };
 let gameId = '0ToP2vQsz2wd4sNV2QT';
 
 const createGame = async () => {
   try {
-    const response = await fetch(`${API_URL}games/`, {
+    const response = await fetch(`${url}games/`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -24,13 +24,14 @@ document.querySelector('.head-1').innerHTML = gameId;
 
 const submitScore = async (user, score) => {
   try {
-    await fetch(`${API_URL}games/${gameId}/scores/`, {
+    const response = await fetch(`${url}games/${gameId}/scores/`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ user, score }),
     });
+    const data = await response.json();
   } catch (error) {
     console.error('Error submitting score:', error);
   }
@@ -41,7 +42,7 @@ const displayLeaderboard = async () => {
   scoreTable.innerHTML = '';
 
   try {
-    const response = await fetch(`${API_URL}games/${gameId}/scores/`);
+    const response = await fetch(`${url}games/${gameId}/scores/`);
     const data = await response.json();
     data.result.forEach((scoreData) => {
       const lineStore = document.createElement('tr');
@@ -56,18 +57,15 @@ const displayLeaderboard = async () => {
   }
 };
 
-const submitScoreData = async () => {
-  const nameInput = document.querySelector('.name');
-  const scoreInput = document.querySelector('.score');
+const submitScoreData = () => {
+  const nameInput = document.querySelector('.name-input');
+  const scoreInput = document.querySelector('.score-input');
 
   if (nameInput && scoreInput) {
     const name = nameInput.value;
     const score = scoreInput.value;
-    await submitScore(name, score);
-    displayLeaderboard(); // Refresh the leaderboard after saving score
-  } else {
-    console.error('Error: Elements with classes "name" and/or "score" not found.');
-  }
+    submitScore(name, score);
+  } 
 };
 
 const submitButton = document.querySelector('.submit-btn');
